a7f744b619c7f3a4237a522ef7c9743a3c85ddc9
cznno <smeraldo.ash@gmail.com>
2017-07-21 10:15:45 +0800

    Initial commit

---
+++ src/main/java/com/fibrlink/parkinglot/ParkinglotManagerApplication.java
@@ -0,0 +1,12 @@
+package com.fibrlink.parkinglot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinglotManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkinglotManagerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/fibrlink/parkinglot/config/ShiroConfiguration.java
@@ -0,0 +1,53 @@
+package com.fibrlink.parkinglot.config;
+
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author Karl Cheng
+ *         Created on 7/20/2017
+ * @version 0.1
+ */
+
+@Configuration
+public class ShiroConfiguration {
+
+    @Bean
+    public ShiroFilterFactoryBean shirFilter(SecurityManager securityManager) {
+
+        System.out.println("ShiroConfiguration.shirFilter()");
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        // 必须设置 SecurityManager
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        //拦截器.
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
+        //配置退出过滤器,其中的具体的退出代码Shiro已经替我们实现了
+        filterChainDefinitionMap.put("/logout", "logout");
+        filterChainDefinitionMap.put("/**", "authc");
+        // login
+        shiroFilterFactoryBean.setLoginUrl("/login");
+        // 登录成功后要跳转的链接
+        shiroFilterFactoryBean.setSuccessUrl("/index");
+        //未授权界面;
+        shiroFilterFactoryBean.setUnauthorizedUrl("/403");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+
+        return shiroFilterFactoryBean;
+    }
+
+    @Bean
+    public SecurityManager securityManager() {
+
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        return securityManager;
+
+    }
+
+
+}
